a15fe85c1b5ce76533a4e4e0c6a79c3f49aa4e4c
tehcanai <80711188+tehcanai@users.noreply.github.com>
2022-01-21 23:39:48 +0800

    Little changes on spelling

--- orison-server/flaskr/db.py
+++ orison-server/flaskr/db.py
@@ -5,6 +5,8 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+System.out.println("Error Test");
+
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
