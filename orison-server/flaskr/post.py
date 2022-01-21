d1a9cbb9b191d44a224e5fa4ac7621958a0ee0b4
tehcanai <80711188+tehcanai@users.noreply.github.com>
2022-01-21 23:07:55 +0800

    Add files via upload

---
+++ orison-server/flaskr/post.py
@@ -0,0 +1,34 @@
+
+from flask import (Blueprint, g, request, render_template, jsonify)
+
+from flaskr.db import get_db, query_db
+
+bp = Blueprint('post', __name__, url_prefix='/post')
+
+@bp.route('/', methods=['GET'])
+def posts_status():
+    db = get_db()
+    available = db.execute('SELECT * FROM post').fetchone()
+
+    if available is None :
+        return "No posts"
+    else :
+        posts = []
+        for post in query_db('SELECT * FROM post'):
+            posts.append({
+            'title': post['title'],
+            'body': post['body'],
+            })
+        return jsonify(posts)
+
+@bp.route('/new', methods=['POST'])
+def store_post():
+    if request.method == 'POST' :
+        post_title = request.form['title']
+        post_body = request.form['body']
+        
+        db = get_db()
+        db.execute('INSERT INTO post (title, body) VALUES (?, ?)', (post_title, post_body))
+        db.commit()
+
+        return "Post sent"
